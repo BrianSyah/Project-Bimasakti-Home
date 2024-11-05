65e8494183382c76bb85f8471bab8d2bea74f8bd
ground1234567 <boybrinsyah@gmail.com>
2024-11-05 22:44:00 +0700

    chor: Baru Navbar, Hero Section

---
+++ src/app/(home)/Card-section.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Cardsection = () => {
+  return (
+    <section className="container max-w-[1130px mx-auto flex flex-col pt-[100px] gap-[30px]">
+      <div className="grid grid-cols-3 gap-[30px]">
+        <div className="flex flex-col gap-[30px"></div>
+      </div>
+    </section>
+  );
+};
+
+export default Cardsection;
